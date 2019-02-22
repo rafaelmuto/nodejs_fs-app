37570446ffb3f403703871c9683f7e851d36c376
Rafael Muto <r.nagahama@gmail.com>
2019-02-21 23:45:29 -0300

    re order of the middlewares...

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+// importing express.js and other modules:
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// importing routes:
+const adminRoutes = require('./routes/adminRouter');
+const shopRoutes = require('./routes/shopRouter');
+
+
 // creating the server(?) obj with the express() function, the function returns an obj:
 const app = express();
 
+
+// =================
+// MIDDLEWARES HERE:
+// =================
 
 // setting up the view engine (pug):
 app.set('view engine', 'pug');
@@ -12,30 +22,27 @@ app.set('view engine', 'pug');
 app.set('views', 'views');
 
 
-// importing routes:
-const adminRoutes = require('./routes/adminRouter');
-const shopRoutes = require('./routes/shopRouter');
-
-// =================
-// MIDDLEWARES HERE:
-// =================
-
 // register the new middleware; bodyParser (imported previously):
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+
 // middleware for serving static filess
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// registering imported routers as midwares:
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+// catch all route for 404 errors:
 app.use((req, res, nxt) => {
     res.status(404).render('404', {
         pageTitle: 'Err404 Page Not Found'
     });
 });
+
 
 // starting the server at port 3000:
 app.listen(3000);
