cd99ac7420627cac7deddc11e322a6c5f115615f
Rafael Muto <r.nagahama@gmail.com>
2019-05-01 12:53:23 -0300

    changed name of mongo node

--- util/database.js
+++ util/database.js
@@ -8,7 +8,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    "mongodb+srv://nodeApp:12345@mdbtest-enper.gcp.mongodb.net/test?retryWrites=true",
+    "mongodb+srv://nodeApp:12345@mdbtest-enper.gcp.mongodb.net/nodejs_app?retryWrites=true",
     // warning: (node:70332) DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
     { useNewUrlParser: true }
   )
