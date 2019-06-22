cb193d58dfe1f99eb692903357d0e8c7b5aff62e
Rafael Muto <r.nagahama@gmail.com>
2019-06-21 23:43:46 -0300

    CORS

--- API/api.js
+++ API/api.js
@@ -16,6 +16,14 @@ const app = express();
 // application/json
 app.use(bodyParser.json());
 
+// middleware to allow CORS (Cross-Origin Resource Sharing):
+app.use((req, res, nxt) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  nxt();
+});
+
 app.use('/feed', feedRouter);
 
 app.get('/', (req, res, nxt) => {
