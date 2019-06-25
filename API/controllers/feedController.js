19e1383c1779e682cf132c788ac373d9119581ba
Rafael Muto <r.nagahama@gmail.com>
2019-06-24 23:31:01 -0300

    update posts

--- API/controllers/feedController.js
+++ API/controllers/feedController.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const postModel = require('../models/postModel');
+const fs = require('fs');
+const path = require('path');
 
 exports.getPosts = (req, res, nxt) => {
   console.log('==> feedController: getPosts');
@@ -79,4 +81,60 @@ exports.getPost = (req, res, nxt) => {
       }
       nxt(err);
     });
+};
+
+exports.updatePost = (req, res, nxt) => {
+  console.log('==> feedController: updatePost');
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const postId = req.params.postId;
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error('No file picked.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  postModel
+    .findById(postId)
+    .then(post => {
+      if (!post) {
+        const err = new Error('Could not fund post.');
+        err.statusCode = 404;
+        throw err;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post updated.', post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      nxt(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
